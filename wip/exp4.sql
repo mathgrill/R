f7f4103b5ffca04fbc6d26e4248ecc32f68db1aa
mathgrill <carlo.baluta@gmail.com>
2020-08-28 16:04:48 -0400

    format code

--- wip/exp4.sql
+++ wip/exp4.sql
@@ -1,90 +1,85 @@
-drop table public.exp4;
-
-create table public.exp4 as
-select 
-case when CPT in ('44146','44208') then 'Yes'
- when  CPT in ('44140','44145','44204','44207') then 'No' end as intervention,
-case when ORGSPCSSI = 'Organ/Space SSI'  then 'Yes'
- when ORGSPCSSI = 'No Complication' then 'No' end as outcome,
- caseid,
-703 * NULLIF (weight, -99) / (NULLIF (height, -99) ^ 2) AS bmi,
-CASE WHEN dopertod >= 0 THEN 'Yes' ELSE 'No' END AS mortality,
-replace(age,'+','')::numeric as age,
-SEX,
-RACE_NEW,
-TOTHLOS,
-DIABETES,
-case when DIABETES = 'NO' then 'No' 
-     when DIABETES is null then null 
-	 else 'Yes' end as DIABETES_bin,
-SMOKE,
-CDMI,
-HXCHF,
-HXCOPD,
-ASCITES,
-RENAINSF,
-OPRENAFL,
-STEROID,
-WTLOSS,
-BLEEDDIS,
-OTHSYSEP,
-ASACLAS,
-DEHIS,
-OUPNEUMO,
-REINTUB,
---OTHBLEED,
-case 
-  when OTHBLEED is null then 'NA'
-  when OTHBLEED = 'No Complication' then OTHBLEED 
-  else 'Yes' 
-end as OTHBLEED,
---OTHDVT,
-case 
-  when OTHDVT is null then 'NA'
-  when OTHDVT = 'No Complication' then OTHDVT 
-  else 'Yes' 
-end as OTHDVT,
-RETURNOR,
-SUPINFEC,
-WNDINFD,
-REOPERATION1,
---selection cols
-PODIAG10,
-WNDCLAS,
-EMERGNCY,
-CPT,
-ventpatos,
-OPTIME,
-case when CPT in ('44204', '44207', '44208') then 'MIS'
-     when CPT in ('44140', '44145', '44146') then 'Open' end as open_or_mis
-from
-public.acs
-where (PODIAG10 = 'K57.20' OR PODIAG = '562.11')
-and WNDCLAS in ('3-Contaminated', '4-Dirty/Infected')
-and EMERGNCY='Yes'
-and CPT in ('44140','44145','44204','44207', '44146','44208')
-and COALESCE(OTHERCPT1, 'x') not in ('44143','44206','44141')
-and COALESCE(OTHERCPT2, 'x') not in ('44143','44206','44141')
-and COALESCE(OTHERCPT3, 'x') not in ('44143','44206','44141')
-and COALESCE(OTHERCPT4, 'x') not in ('44143','44206','44141')
-and COALESCE(OTHERCPT5, 'x') not in ('44143','44206','44141')
-and COALESCE(OTHERCPT6, 'x') not in ('44143','44206','44141')
-and COALESCE(OTHERCPT7, 'x') not in ('44143','44206','44141')
-and COALESCE(OTHERCPT8, 'x') not in ('44143','44206','44141')
-and COALESCE(OTHERCPT9, 'x') not in ('44143','44206','44141')
-and COALESCE(OTHERCPT10, 'x') not in ('44143','44206','44141')
---
-and COALESCE(CONCPT1, 'x')  not in ('44143','44206','44141')
-and COALESCE(CONCPT2, 'x') not in ('44143','44206','44141')
-and COALESCE(CONCPT3, 'x') not in ('44143','44206','44141')
-and COALESCE(CONCPT4, 'x') not in ('44143','44206','44141')
-and COALESCE(CONCPT5, 'x') not in ('44143','44206','44141')
-and COALESCE(CONCPT6, 'x') not in ('44143','44206','44141')
-and COALESCE(CONCPT7, 'x') not in ('44143','44206','44141')
-and COALESCE(CONCPT8, 'x') not in ('44143','44206','44141')
-and COALESCE(CONCPT9, 'x') not in ('44143','44206','44141')
-and COALESCE(CONCPT10, 'x') not in ('44143','44206','44141')
-and COALESCE(OTHSESHOCK, 'x') != 'Septic Shock'
-and COALESCE(ASACLAS, 'x') != '5-Moribund'
-and coalesce(ventpatos, 'x') not in ('Yes')
-;
+DROP 
+  TABLE public.exp4;
+CREATE TABLE public.exp4 AS 
+SELECT 
+  CASE WHEN cpt IN ('44146', '44208') THEN 'Yes' WHEN cpt IN (
+    '44140', '44145', '44204', '44207'
+  ) THEN 'No' END AS intervention, 
+  CASE WHEN orgspcssi = 'Organ/Space SSI' THEN 'Yes' WHEN orgspcssi = 'No Complication' THEN 'No' END AS outcome, 
+  caseid, 
+  703 * NULLIF (weight, -99) / (
+    NULLIF (height, -99) ^ 2
+  ) AS bmi, 
+  CASE WHEN dopertod >= 0 THEN 'Yes' ELSE 'No' END AS mortality, 
+  replace(age, '+', ''):: NUMERIC AS age, 
+  sex, 
+  race_new, 
+  tothlos, 
+  diabetes, 
+  CASE WHEN diabetes = 'NO' THEN 'No' WHEN diabetes IS NULL THEN NULL ELSE 'Yes' END AS diabetes_bin, 
+  smoke, 
+  cdmi, 
+  hxchf, 
+  hxcopd, 
+  ascites, 
+  renainsf, 
+  oprenafl, 
+  steroid, 
+  wtloss, 
+  bleeddis, 
+  othsysep, 
+  asaclas, 
+  dehis, 
+  oupneumo, 
+  reintub, 
+  CASE WHEN othbleed IS NULL THEN 'NA' WHEN othbleed = 'No Complication' THEN othbleed ELSE 'Yes' END AS othbleed, 
+  CASE WHEN othdvt IS NULL THEN 'NA' WHEN othdvt = 'No Complication' THEN othdvt ELSE 'Yes' END AS othdvt, 
+  returnor, 
+  supinfec, 
+  wndinfd, 
+  reoperation1, 
+  podiag10, 
+  wndclas, 
+  emergncy, 
+  cpt, 
+  ventpatos, 
+  optime, 
+  CASE WHEN cpt IN ('44204', '44207', '44208') THEN 'MIS' WHEN cpt IN ('44140', '44145', '44146') THEN 'Open' END AS open_or_mis 
+FROM 
+  public.acs 
+WHERE 
+  (
+    podiag10 = 'K57.20' 
+    OR podiag = '562.11'
+  ) 
+  AND wndclas IN (
+    '3-Contaminated', '4-Dirty/Infected'
+  ) 
+  AND emergncy = 'Yes' 
+  AND cpt IN (
+    '44140', '44145', '44204', '44207', 
+    '44146', '44208'
+  ) 
+  AND COALESCE(othercpt1, 'x') NOT IN ('44143', '44206', '44141') 
+  AND COALESCE(othercpt2, 'x') NOT IN ('44143', '44206', '44141') 
+  AND COALESCE(othercpt3, 'x') NOT IN ('44143', '44206', '44141') 
+  AND COALESCE(othercpt4, 'x') NOT IN ('44143', '44206', '44141') 
+  AND COALESCE(othercpt5, 'x') NOT IN ('44143', '44206', '44141') 
+  AND COALESCE(othercpt6, 'x') NOT IN ('44143', '44206', '44141') 
+  AND COALESCE(othercpt7, 'x') NOT IN ('44143', '44206', '44141') 
+  AND COALESCE(othercpt8, 'x') NOT IN ('44143', '44206', '44141') 
+  AND COALESCE(othercpt9, 'x') NOT IN ('44143', '44206', '44141') 
+  AND COALESCE(othercpt10, 'x') NOT IN ('44143', '44206', '44141') 
+  AND COALESCE(concpt1, 'x') NOT IN ('44143', '44206', '44141') 
+  AND COALESCE(concpt2, 'x') NOT IN ('44143', '44206', '44141') 
+  AND COALESCE(concpt3, 'x') NOT IN ('44143', '44206', '44141') 
+  AND COALESCE(concpt4, 'x') NOT IN ('44143', '44206', '44141') 
+  AND COALESCE(concpt5, 'x') NOT IN ('44143', '44206', '44141') 
+  AND COALESCE(concpt6, 'x') NOT IN ('44143', '44206', '44141') 
+  AND COALESCE(concpt7, 'x') NOT IN ('44143', '44206', '44141') 
+  AND COALESCE(concpt8, 'x') NOT IN ('44143', '44206', '44141') 
+  AND COALESCE(concpt9, 'x') NOT IN ('44143', '44206', '44141') 
+  AND COALESCE(concpt10, 'x') NOT IN ('44143', '44206', '44141') 
+  AND COALESCE(othseshock, 'x') != 'Septic Shock' 
+  AND COALESCE(asaclas, 'x') != '5-Moribund' 
+  AND COALESCE(ventpatos, 'x') NOT IN ('Yes');
